856b2737c9233b9e636fb0e243c429a2871f031f
Yash <yash.mehta@jklgroup.in>
2023-09-11 11:53:59 +0530

    added example for user sign up and login response in swagger

--- src/users/dto/response/login-user.dto.ts
+++ src/users/dto/response/login-user.dto.ts
@@ -1,15 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class UserResDto {
   @Expose()
+  @ApiProperty({
+    example: '1'
+  })
   id: number;
 
   @Expose()
+  @ApiProperty({
+    example: 'John'
+  })
   firstName: string;
 
   @Expose()
+  @ApiProperty({
+    example: 'Doe'
+  })
   lastName: string;
 
   @Expose()
+  @ApiProperty({
+    example: 'john@doe.com'
+  })
   email: string;
 }
