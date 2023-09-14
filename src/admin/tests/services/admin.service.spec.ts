c6aa079e2684239fbd1fab53ec38feab9b8f4ef8
Yash <yash.mehta@jklgroup.in>
2023-09-14 09:47:15 +0530

    added test case for failed admin profile

--- src/admin/tests/services/admin.service.spec.ts
+++ src/admin/tests/services/admin.service.spec.ts
@@ -7,6 +7,7 @@ import { TokenService } from '../../../utils/token/services';
 import { mockAdminRepository } from '../mocks/admin.repository.mock';
 import { UserType } from '../../../utils/token/types';
 import { AdminProfileReqDto } from '../../dto';
+import { NotFoundException } from '../../errors';
 
 describe('AdminService', () => {
   let adminService: AdminService;
@@ -39,30 +40,45 @@ describe('AdminService', () => {
   });
 
   describe('Profile Test', () => {
-    describe('Get Profile where ID exists', () => {
-      it('Should Return Profile Object', async () => {
-        const body: AdminProfileReqDto = {
-          id: '929270f8-f62e-4580-8533-10d473ce520a',
-          userType: UserType.ADMIN,
-          email: 'john@doe.com',
-          iat: 1234,
-          exp: 1234,
-        };
-        const expected = {
-          id: '929270f8-f62e-4580-8533-10d473ce520a',
-          firstName: 'john',
-          lastName: 'doe',
-          email: 'john@doe.com',
-          password:
-            '$2b$10$4Dz7cd/nTzDm2Dm2vRbYs.SQUtRrV2pE/Z7L82XataOOJklLPiM.2',
-          createdAt: '2023-09-13T01:41:57.449Z',
-          updatedAt: '2023-09-13T01:41:57.449Z',
-          deletedAt: null,
-        };
-        adminRepository.getById.mockReturnValue(expected);
-        const user = await adminService.profile(body);
-        expect(user).toEqual(expected);
-      });
+    it('Get Profile where ID exists Should Return Profile Object', async () => {
+      const body: AdminProfileReqDto = {
+        id: '929270f8-f62e-4580-8533-10d473ce520a',
+        userType: UserType.ADMIN,
+        email: 'john@doe.com',
+        iat: 1234,
+        exp: 1234,
+      };
+      const expected = {
+        id: '929270f8-f62e-4580-8533-10d473ce520a',
+        firstName: 'john',
+        lastName: 'doe',
+        email: 'john@doe.com',
+        password:
+          '$2b$10$4Dz7cd/nTzDm2Dm2vRbYs.SQUtRrV2pE/Z7L82XataOOJklLPiM.2',
+        createdAt: '2023-09-13T01:41:57.449Z',
+        updatedAt: '2023-09-13T01:41:57.449Z',
+        deletedAt: null,
+      };
+      adminRepository.getById.mockReturnValue(expected);
+      const user = await adminService.profile(body);
+      expect(user).toEqual(expected);
+    });
+    it('Get Profile where ID Doesnt Exists Should throw NotFoundException ', async () => {
+      const body: AdminProfileReqDto = {
+        id: '929270f8-f62e-4580-8533-10d473ce520a',
+        userType: UserType.ADMIN,
+        email: 'john@doe.com',
+        iat: 1234,
+        exp: 1234,
+      };
+  
+      adminRepository.getById.mockRejectedValue(new NotFoundException());
+      try {
+        await adminService.profile(body);
+        fail('NotFoundException not thrown');
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
     });
   });
 });
